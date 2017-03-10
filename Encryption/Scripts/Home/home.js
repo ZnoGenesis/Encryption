08fe9844e6565714c51ed0f088d9fea70da012ce
ratchawut <ratchawutk55@email.nu.ac.th>
2017-03-10 14:19:59 +0700

    update code:

--- Encryption/Scripts/Home/home.js
+++ Encryption/Scripts/Home/home.js
@@ -34,14 +34,20 @@ app.controller("homeCtrl", function ($scope, $http) {
             {
                 id: 1,
                 name: 'GITS',
+                key: "Q29ubmV4Nzg=",
+                vector: "TXVuZXo2Nzg="
+            },
+            {
+                id: 2,
+                name: 'Appctrl',
                 key: "Z2J4YXBwY3Q=",
                 vector: "Z2l0czIwMDk="
             },
             {
-                id: 2,
-                name: '-----------------',
-                key: "NOKEY",
-                vector: "NO_VECTOR"
+                id: 3,
+                name: 'Backoffice',
+                key: "Q29ubmV4Nzg=",
+                vector: "TXVuZXo2Nzg="
             }
         ]
         return list
@@ -49,7 +55,7 @@ app.controller("homeCtrl", function ($scope, $http) {
     $scope.pj = getDropdown()
     $scope.pjx = getDropdown()
     $scope.encryp = function () {
-        //console.log($scope.encrypt);
+        console.log($scope.encrypt);
         $http.post("/Encryption/Encrypt", $scope.encrypt)
             .success(function (response) {
                 $scope.encrypOutput = response;
@@ -57,7 +63,7 @@ app.controller("homeCtrl", function ($scope, $http) {
     }
 
     $scope.decryp = function () {
-       
+        console.log($scope.decrypt)
         $http.post("/Encryption/Decrypt", $scope.decrypt)
             .success(function (response) {
                 $scope.decrypOutput = response;
