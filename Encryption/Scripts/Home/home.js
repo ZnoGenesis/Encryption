2e79014a1317b7858bc291665c1579bc0e433da0
ratchawut <ratchawutk55@email.nu.ac.th>
2017-03-10 11:50:20 +0700

    add decode endcode

--- Encryption/Scripts/Home/home.js
+++ Encryption/Scripts/Home/home.js
@@ -2,17 +2,17 @@
 
 app.controller("homeCtrl", function ($scope, $http) {
 
-   
-    
+
+
     $scope.converter = {
-        encode: function (){
+        encode: function () {
             $http({
                 url: "/Encryption/Base64Encode",
                 method: "POST",
                 params: {
                     plainText: $scope.encode_input
                 }
-            }).success(function (data) {                        
+            }).success(function (data) {
                 $scope.encode = data
             })
         },
@@ -28,70 +28,43 @@ app.controller("homeCtrl", function ($scope, $http) {
             })
         }
     }
-    $scope.name = "top"
 
-    $scope.pj = [
-        {
-            id: 1,
-            name: 'GITS',
-            key: "Z2J4YXBwY3Q=",
-            vector: "Z2l0czIwMDk="
-        },
-        {
-            id : 2,
-            name:'' ,
-            key: "NOKEY",
-            vector: "NO VECTOR"
-        }
-    ]
-    console.log($scope.pj)
-    $scope.project = [
-        {
-            id:0,
-            name: "AppCtrl",
-            key: [
-                    { key: "Z2J4YXBwY3Q=", vector: "vector AppCtrl" },
-                    { key: "Z2J4YXBwY3Q=", vector: "vector AppCtrl" },
-                    { key: "Z2J4YXBwY3Q=", vector: "vector AppCtrl" }
-            ]
-        },
-        {
-            id:1,
-            name: "GITS",
-            key: [
-                9,
-                8,
-                7
-            ]
-        }
-    ]
-    $scope.key = [
-        { id: "1", key: "Z2J4YXBwY3Q=" },
-        { id: "2", key: "Z2J4YXBwY3Q=" },
-        { id: "3", key: "Z2J4YXBwY3Q=" }
-    ];
-    $scope.vector = [
-        { id: "1", key: "Z2l0czIwMDk=" },
-        { id: "2", key: "Z2l0czIwMDk=" },
-        { id: "3", key: "Z2l0czIwMDk=" }
-    ];
-    $scope.Vector = ["Z2l0czIwMDk="];
+    function getDropdown() {
+        var list = [
+            {
+                id: 1,
+                name: 'GITS',
+                key: "Z2J4YXBwY3Q=",
+                vector: "Z2l0czIwMDk="
+            },
+            {
+                id: 2,
+                name: '-----------------',
+                key: "NOKEY",
+                vector: "NO_VECTOR"
+            }
+        ]
+        return list
+    }
+    $scope.pj = getDropdown()
+    $scope.pjx = getDropdown()
     $scope.encryp = function () {
-        console.log($scope.encrypt);
+        //console.log($scope.encrypt);
         $http.post("/Encryption/Encrypt", $scope.encrypt)
             .success(function (response) {
                 $scope.encrypOutput = response;
-            });       
+            });
     }
-    $scope.mydecrypt = function () {
-        console.log($scope.objdecrypt);
-        $http.post("/Encryption/Decrypt", $scope.objdecrypt)
+
+    $scope.decryp = function () {
+       
+        $http.post("/Encryption/Decrypt", $scope.decrypt)
             .success(function (response) {
                 $scope.decrypOutput = response;
             });
     }
 
-    $scope.isFalse = function () {        
+    $scope.isFalse = function () {
         return $scope.encrypt.key == null ? true : false;
     }
     $scope.dataIsNull = function () {
