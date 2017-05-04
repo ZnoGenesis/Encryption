cb09bc3e75d5dd8be6e66c92f7793aba22b5c63c
ratchawut <ratchawutk55@email.nu.ac.th>
2017-05-04 17:06:28 +0700

    restore code to master

---
+++ Encryption/Models/Encrypt.cs
@@ -0,0 +1,16 @@
+﻿using AppCtrl.Encryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Encryption.Models
+{
+    public class Encrypt
+    {
+        public string Key { get; set; }
+        public string Vector { get; set; }
+        public string Data { get; set; }
+        public string level { get; set; }
+    }
+}
